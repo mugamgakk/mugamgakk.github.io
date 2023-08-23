6cebd779390473bb13799cd0348e33c9a1b9ed03
mugamgakk <88496769+mugamgakk@users.noreply.github.com>
2023-08-23 22:08:41 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(event) {
+    // 캐싱 처리
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
